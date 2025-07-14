c1a444869cf1cee92f8328c653b30bc39da0321b
marimari20020711 <marimari20020711@gmail.com>
2025-07-13 23:40:46 -0700

    db:truncate_allをbin/render-build.shに追加

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,9 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+# 必要に応じてデータ削除
 bundle exec rails db:truncate_all
+
 bundle exec rails db:migrate
 bundle exec rails import:sangiin_members
 bundle exec rails import:shugiin_members
