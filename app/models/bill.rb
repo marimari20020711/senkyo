d0bcea1bbef875450eec7f507cda9f55c6c34f46
marimari20020711 <marimari20020711@gmail.com>
2025-08-30 14:42:00 +0900

    fix: billバリデーションからnumberカラムを削除

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,5 +1,6 @@
 class Bill < ApplicationRecord
   has_many :bill_supports, dependent: :destroy
+  validates :session, :title, :kind, presence: true
 
   def self.ransackable_attributes(auth_object = nil)
     %w[title]
