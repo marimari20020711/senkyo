89875044488439a27492cc30c9558f2d3dbb97ed
marimari20020711 <marimari20020711@gmail.com>
2025-09-13 20:01:05 +0900

    fix: "$RESET_DB" = "true" に設定でDB をリセットするよう修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,16 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
 # データだけ消して再投入
-# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:truncate_all
+#Dashboard → Environment → Environment Variables で、Key: RESET_DB、Value: true を設定すると、DBリセットモードで動作します
+if [ "$RESET_DB" = "true" ]; then
+  echo "⚠️ RESET_DB=true が設定されているので DB をリセットします"
+  DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create db:migrate
+else
+  echo "➡️ 通常モード: 既存DBを保持して migrate のみ実行"
+  bundle exec rails db:migrate
+fi
 
 bundle exec rails db:migrate
 bundle exec rails import:sangiin_members
