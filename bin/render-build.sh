5dcb03f7b85068d842c13023179a3ecf0b3f79f9
marimari20020711 <marimari20020711@gmail.com>
2025-09-13 19:44:07 +0900

    update:

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,11 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-# 必要に応じてデータ削除
+# データだけ消して再投入
 # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:truncate_all
+# DBをゼロから作り直す（スキーマ作成 + seed実行）
+bundle exec rails db:setup
+
 bundle exec rails db:migrate
 bundle exec rails import:sangiin_members
 bundle exec rails import:shugiin_members
